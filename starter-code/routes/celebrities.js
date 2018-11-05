9756a6d4aabb31d1e91e6d0ac3372f81671469df
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-05 11:59:24 +0100

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,7 +10,22 @@ router.get('/', (req, res, next) => {
     .then((celebritiesFromDb) => {
       res.render('celebrities/index', { celebritiesFromDb });
     })
-    .catch(err => next(err));
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+/* GET celebrity page */
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  Celebrities.findById(id)
+    .then((celebrityFromDb) => {
+      res.render('celebrities/show', { celebrityFromDb });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
