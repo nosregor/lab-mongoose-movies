1dd0a2f17f6a30dc4a90f674c8e0f4763c57bebd
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-05 14:49:43 +0100

    Iteration 5 & 6

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,34 +1,45 @@
 const express = require('express');
 // add model
-const Celebrities = require('../models/Celebrity');
+const Celebrity = require('../models/Celebrity');
 
 const router = express.Router();
 
-/* GET celebrities page */
+/* GET all celebrities
+celebrities/index.html */
 router.get('/', (req, res, next) => {
-  Celebrities.find({})
-    .then((celebritiesFromDb) => {
-      res.render('celebrities/index', { celebritiesFromDb });
+  Celebrity.find({})
+    .then((celebrities) => {
+      res.render('celebrities/index', { celebrities });
     })
     .catch((error) => {
       console.log(error);
     });
 });
 
-/* route displays the POST form */
+/* render form */
 router.get('/new', (req, res, next) => {
   res.render('celebrities/new');
 });
 
-
+// Create a celebrity with the information from the form
 router.post('/new', (req, res, next) => {
   const { name, occupation, catchPhrase } = req.body;
+  // If there is an empty title
+  // if (!name) {
+  //   res.render('new', {
+  //     error: 'The name must be filled',
+  //   });
+  //   return;
+  // }
+  // this is one option
+  // Celebrities.create({
+  //   name,
+  //   occupation,
+  //   catchPhrase,
+  // })
 
-  Celebrities.create({
-    name,
-    occupation,
-    catchPhrase,
-  })
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+  newCelebrity.save()
     .then((celebrity) => {
       res.redirect(`/celebrities/${celebrity._id}`);
     })
@@ -38,18 +49,59 @@ router.post('/new', (req, res, next) => {
 });
 
 
-/* GET celebrity page */
+/* Show details of a celebrity */
+// celebrities/:id
 router.get('/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  Celebrities.findById(id)
-    .then((celebrityFromDb) => {
-      res.render('celebrities/show', { celebrityFromDb });
+  Celebrity.findById(req.params.id)
+    .then((celebrity) => {
+      res.render('celebrities/show', { celebrity });
     })
     .catch((error) => {
-      console.log(error);
+      next(error);
     });
 });
 
+// render the edit form
+router.get('/:id/edit', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then((celebrity) => {
+      console.log('From the edit get route:', celebrity);
+      res.render('celebrities/edit', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Update the celebrity with info from the form
+router.post('/:id/edit', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  // Find the book and update it with the info from the form
+  Celebrity.findByIdAndUpdate(req.params.id, {
+    name,
+    occupation,
+    catchPhrase,
+  })
+    .then((celebrity) => {
+      console.log(`Updated celebrity: ${req.params.id}`);
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Delete celebrity
+router.get('/:id/delete', (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      console.log('ERROR');
+      next(error);
+    });
+});
 
 module.exports = router;
