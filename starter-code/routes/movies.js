d1faf9ebcbdc1d259384f85bc7b1cdf756e6f0df
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-05 15:37:06 +0100

    Iteration 10

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -16,6 +16,31 @@ router.get('/', (req, res, next) => {
     });
 });
 
+/* render form */
+router.get('/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+// Create a celebrity with the information from the form
+router.post('/new', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  // If there is an empty title
+  if (!title) {
+    res.render('new', {
+      error: 'The title must be filled',
+    });
+    return;
+  }
+  Movie.create({ title, genre, plot })
+    .then((movie) => {
+      res.redirect(`/movies/${movie._id}`);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+
 router.get('/:id', (req, res, next) => {
   Movie.findById(req.params.id)
     .then((movie) => {
