c6a9500e4bd9919f934b97fc036af3ce144b6ec9
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-05 15:46:47 +0100

    Iteration 11

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -4,7 +4,7 @@ const Movie = require('../models/Movie');
 
 const router = express.Router();
 
-/* GET all celebrities
+/* GET all movies
 movies/index.html */
 router.get('/', (req, res, next) => {
   Movie.find()
@@ -40,7 +40,6 @@ router.post('/new', (req, res, next) => {
     });
 });
 
-
 router.get('/:id', (req, res, next) => {
   Movie.findById(req.params.id)
     .then((movie) => {
@@ -51,5 +50,48 @@ router.get('/:id', (req, res, next) => {
     });
 });
 
+
+// render the edit form
+router.get('/:id/edit', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      console.log('From the edit get route:', movie);
+      res.render('movies/edit', { movie });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Update the celebrity with info from the form
+router.post('/:id/edit', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+
+  // Find the book and update it with the info from the form
+  Movie.findByIdAndUpdate(req.params.id, {
+    title,
+    genre,
+    plot,
+  })
+    .then((movie) => {
+      console.log(`Updated movie: ${req.params.id}`);
+      res.redirect('/movies');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Delete celebrity
+router.get('/:id/delete', (req, res, next) => {
+  Movie.findByIdAndRemove(req.params.id)
+    .then((movie) => {
+      res.redirect('/movies');
+    })
+    .catch((error) => {
+      console.log('ERROR');
+      next(error);
+    });
+});
 
 module.exports = router;
